fbc94b5dabcd969278a407a90a2e4b3eaa00cf3b
Atharv Kachare <104071489+Atharvkachare@users.noreply.github.com>
2024-05-10 18:48:09 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Name {
+
+    void f(i,n){
+        if(i>n){
+            return;
+        }
+        System.out.println("Raj");
+        f(i+1,n);
+
+
+
+  
+    }
+
+
+    public static void main(String[] args) {
+        int n;
+        Scanner n = new Scanner(System.in);
+        f(i, n);
+    }
+    
+}
